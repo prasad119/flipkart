0abea6b057a2f0eb375d5c15dc0d20230b5a97e6
Sai Lakshmi Prasad Uppalapati <119niki@gmail.com>
2015-10-24 00:48:39 +0530

    browser changed to ie

--- flipkart/src/com/flipkart/functions/Global.java
+++ flipkart/src/com/flipkart/functions/Global.java
@@ -7,7 +7,7 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 public class Global {
 	public static WebDriver driver;
 	public static String url = "http://www.flipkart.com/";
-	public static String browser = "firefox";
+	public static String browser = "ie";
 	public static String errMsg = null;
 	public static WebDriverWait wait;
 }
